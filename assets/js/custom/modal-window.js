038ecbaad04e7cd82bfdcaa77e89fce3cab4e04d
pedro-digid <pedro@dig.id>
2022-05-09 16:57:00 +0100

    up

--- assets/js/custom/modal-window.js
+++ assets/js/custom/modal-window.js
@@ -1,23 +1,27 @@
 $(function() {
   var blurObjects = $( '.box-header, .box-main-content' );
+  var blurBG = $( '.box-wrapper--image' );
   var modal = $( '.modal-window' );
   var btnOpen = $( '.modal-open' );
   var btnClose = $( '.modal-close' );
   btnOpen.on( 'click', function(e) {
     e.preventDefault();
     modal.addClass('active');
+    blurBG.addClass('active');
     blurObjects.addClass('filter-blur');
   });
 
   btnClose.on( 'click', function(e) {
     e.preventDefault();
     modal.removeClass('active');
+    blurBG.removeClass('active');
     blurObjects.removeClass('filter-blur');
   });
 
   $(window).on( 'click', function(e) {
     if (e.target == modal) {
       modal.removeClass('active');
+      blurBG.removeClass('active');
       blurObjects.removeClass('filter-blur');
     }
   });
